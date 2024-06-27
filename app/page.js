dbcb528ea718c3816dd43f490b68a771b007319b
Paul Caine <paulcaine@tutanota.com>
2024-06-27 22:57:18 +0100

    many changes

--- app/page.js
+++ app/page.js
@@ -34,10 +34,10 @@ export default function Home() {
   const fakeCidrNotation = CIDR();
 
   return (
-    <div className="container max-w-1/2 m-auto bg-slate-900 text-gray-200 p-10 mt-10 md:flex gap-10">
+    <div className="container max-w-1/2 m-auto text-gray-200 p-10 mt-10 md:flex gap-10">
       <div className="w-full md:w-2/3">
         <h1 className="text-4xl text-purple-700 font-extrabold">
-          Random IP Gen
+          Random IP Gen v1
         </h1>
 
         <p className="py-10">
@@ -53,23 +53,46 @@ export default function Home() {
         <h2 className="px-8 py-3 bg-gray-700 text-orange-500 w-fit font-2xl bg-opacity-10 shadow-md my-10">
           {fakeIPAddress} / {fakeCidrNotation}
         </h2>
+        <div className="flex gap-3">
+          <a
+            className="bg-green-500 text-gray-50 px-5 py-3 rounded-md hover:bg-green-400 hover:shadow-md text-center"
+            href="/"
+          >
+            Refresh IP
+          </a>
 
-        <a
-          className="bg-green-500 text-gray-50 px-5 py-3 rounded-md hover:bg-green-400 hover:shadow-md"
-          href="/"
-        >
-          Refresh IP
-        </a>
+          <Link
+            className="bg-cyan-500 text-gray-50 px-5 py-3 rounded-md hover:bg-cyan-400 hover:shadow-md text-center"
+            href="/subnetting_chill.docx"
+          >
+            Download Printout
+          </Link>
+        </div>
+
+        <div className="mt-8">
+          <p>
+            Check your work with the subnet calculator @{" "}
+            <Link
+              className="text-cyan-400 hover:text-cyan-100"
+              href="https://www.calculator.net/ip-subnet-calculator.html"
+              target="blank"
+            >
+              Subnet Calculator
+            </Link>
+          </p>
+        </div>
 
         <div className="mt-10">
-          <p>I will look in to ass CIDR notation soon as I get 5 🫶 💀</p>
-          <p>Im also going to add a print out verson. </p>
+          <h2>Changes</h2>
+          <ul>
+            <li>:: Added random IP gen and CIDR notation.</li>
+          </ul>
         </div>
       </div>
 
       <div className="md:pt-0 pt-10">
         <h2 className="text-2xl pb-5 text-purple-700 font-extrabold">Links</h2>
-        <div className="grid grid-cols-4 gap-4">
+        <div className="flex gap-4">
           <Link href="https://github.com/IAmCainey" target="blank">
             <FaGithub className="text-2xl" />
           </Link>
