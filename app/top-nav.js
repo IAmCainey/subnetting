7e3b1eebc9a24188bc19687c991dc290cf40585d
Paul Caine <paulcaine@tutanota.com>
2024-08-07 00:41:43 +0100

    added link for password gen

--- app/top-nav.js
+++ app/top-nav.js
@@ -5,6 +5,7 @@ export default function TopNav() {
     <div class="flex m-auto w-3/4 py-5 gap-10">
       <LINK href="/">Subnetting</LINK>
       <LINK href="/">OSI Model</LINK>
+      <LINK href="/password">Password Gen</LINK>
       <LINK href="/">About</LINK>
     </div>
   );
